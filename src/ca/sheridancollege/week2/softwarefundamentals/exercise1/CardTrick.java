d26c8666d0d9ada5dcbdc192edb29dc4391e9396
Meet <meetrachhadia910@gmail.com>
2020-05-28 16:28:39 -0400

    Exercise 1 Completed

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -5,11 +5,16 @@
  */
 package ca.sheridancollege.week2.softwarefundamentals.exercise1;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
+ * @modifier Meetkumar Rachhadia - 991587874
  */
 public class CardTrick {
     
@@ -17,16 +22,43 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        Random rand = new Random();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            c.setValue((int) (Math.random() * (13) + 1));
+            c.setSuit(Card.SUITS[rand.nextInt(4)]);
+            
+            System.out.println(c.getValue());
+            System.out.println(c.getSuit());
+            
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner in = new Scanner(System.in);
+        
+        Card userCard = new Card();
+        
+        System.out.println("Enter your guess value(1-13) and suit( Hearts, Diamonds, Clubs, Spades):");
+        int guessValue = in.nextInt();
+        userCard.setValue(guessValue);
+        String guessSuit = in.next();
+        userCard.setSuit(guessSuit);
+        boolean report = false;
+        
         // and search magicHand here
+        for (int i=0; i<magicHand.length; i++){
+            if(userCard.getValue() == magicHand[i].getValue() && userCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit())){
+                report = true;
+                break;
+            }
+        }
+        
         //Then report the result here
+        System.out.println("The user won: " + report);
+        
     }
     
 }
