57b0d466663bb0649acd315fa835bbe7870b8798
parne <parne@192.168.0.12>
2020-05-29 17:26:41 -0400

    second Commit

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -11,19 +11,42 @@ package ca.sheridancollege.week2.softwarefundamentals.exercise1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
  */
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner sc=new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            magicHand[i]=c;//c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        System.out.println("enter the suit");//insert code to ask the user for Card value and suit, create their card
+        String suit= sc.nextLine();
+        System.out.println("enter the value");
+        int value=sc.nextInt();
+
+        String guess="";
         
+        for(int i=0;i<magicHand.length-1;i++)
+        {
+           if((magicHand[i].getValue()==value)&&(magicHand[i].getSuit().equals(suit)))
+                   {
+                        guess="you win";
+                   }
+           else
+                  {
+               guess="you loose";
+               
+                  }           
+        }
+         System.out.println("report="+guess);
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
