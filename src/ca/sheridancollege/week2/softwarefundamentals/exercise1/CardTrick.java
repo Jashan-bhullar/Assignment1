dbd5f9d16b0190297ee86d16aebaa94dfe6696fb
Arashdeep Singh <>
2020-05-30 20:32:36 -0400

    2st commit

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -17,8 +17,6 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         Scanner input=new Scanner(System.in);
-        int userChoiceOfNumber=0;
-        String userChoiceOfSuit="";
         for (int i=0; i<magicHand.length; i++)
         {
             int value;
@@ -35,23 +33,16 @@ public class CardTrick {
             
         }
         
-        
         //insert code to ask the user for Card value and suit, create their card
         Card luckyCard = new Card();
         luckyCard.setValue(10);
         luckyCard.setSuit("Diamonds");
-        System.out.println("Guess any number from the deck of 52 cards."
-                + "\nEnter card Suit here:");
-        userChoiceOfSuit=input.next();
-        System.out.println("Enter number of card.");
-        userChoiceOfNumber=input.nextInt();
-        
         
         // and search magicHand here
         boolean isWon=false;
         for(int i=0;i<magicHand.length;i++){
-            if(magicHand[i].getValue()==userChoiceOfNumber&&
-                    magicHand[i].getSuit().equalsIgnoreCase(userChoiceOfSuit)){
+            if(magicHand[i].getValue()==luckyCard.getValue()&&
+                    magicHand[i].getSuit().equalsIgnoreCase(luckyCard.getSuit())){
                 isWon=true;
             }
             else isWon=false;
