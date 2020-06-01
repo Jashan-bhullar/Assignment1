fac78819c4b0a387cc7b8247a758ad0ede533c1d
Gurkirat <gurkirat2773@gmail.com>
2020-06-01 00:54:22 -0400

    First commit

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -1,9 +1,14 @@
+/*Gurkirat Singh
+  student number-991592262
+*/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package ca.sheridancollege.week2.softwarefundamentals.exercise1;
+
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -19,14 +24,33 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
+           Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random()*13) + 1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            int temp = (int)(Math.random() * 4);
+            c.setSuit(c.SUITS[temp]);
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+          Scanner input = new Scanner(System.in);
+        System.out.println("Choose any card number from 1 to 13");
+        System.out.print("Card Number : ");
+        int cardNumber = input.nextInt();
+        System.out.print("Suit : ");
+        String suit = input.next();
+        Card userCard = new Card();
+        userCard.setValue(cardNumber);
+        userCard.setSuit(suit);
+        boolean result = false;
+        for(int i = 0;i<magicHand.length;i++)
+        {
+            if(userCard.equals(magicHand[i]));
+              result = true;
+       
     }
     
-}
+    }}
