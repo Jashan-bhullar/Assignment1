4312400bfc5110d0add5eb18245873cfb1d20655
kaur8865 <65926707+kaur8865@users.noreply.github.com>
2020-06-02 19:50:14 -0400

    Taking User Input

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -1,4 +1,4 @@
-/*
+d/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -20,13 +20,49 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+          //c.setValue(insert call to random number generator here)
+            c.setValue((int)(1+Math.random()*13));
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            
+             magicHand[i]=c;
+             
+             System.out.println("Value="+magicHand[i].getValue()+" and suit="+magicHand[i].getSuit());
         }
-        
+     
         //insert code to ask the user for Card value and suit, create their card
+        Scanner sc=new Scanner(System.in); 
+        System.out.println("Enter the card value and suit");
+        int value=sc.nextInt();
+        String suit=sc.next();
+        
+        int temp=0;
+        
+        Card c=new Card();
+        c.setValue(value);
+        c.setSuit(suit);
+     
+        String result=" ";
+        for(int i=0;i<magicHand.length;i++)
+        {
         // and search magicHand here
-        //Then report the result here
+            if(value==magicHand[i].getValue()&&suit.equalsIgnoreCase(magicHand[i].getSuit()))
+            {
+                result="You have got a magic hand.";
+                temp=1;
+                break;
+            }
+        }
+            if(temp==0)
+            {
+                result="You didn't get the magic hand.";
+            }
+          //Then report the result here
+        System.out.println(result);  
+            
+        
+        
+        
     }
     
 }
