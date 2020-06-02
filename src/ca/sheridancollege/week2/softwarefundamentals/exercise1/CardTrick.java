dd6b476c59c534a7da3c783310f9fe82ece585a1
trung tran <tranthe@sheridancollege.ca>
2020-06-02 08:35:36 -0400

    Implement CardTrick class first commit

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -5,28 +5,82 @@
  */
 package ca.sheridancollege.week2.softwarefundamentals.exercise1;
 
+
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author Trung Tran
+ * Student ID: 991536629
  */
 public class CardTrick {
-    
+
     public static void main(String[] args)
     {
+        int cardValue, cardSuit;
+
         Card[] magicHand = new Card[7];
-        
+
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.ceil(Math.random() * 13)));
+            c.setSuit(Card.SUITS[(int)(Math.floor(Math.random() * 4))]);
+            magicHand[i] = c;
         }
-        
+
         //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        Scanner input = new Scanner(System.in);
+        loop: while(true) {
+            System.out.println("Enter card's value: ");
+            cardValue = input.nextInt();
+            switch(cardValue) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                    break loop;
+                default:
+            }
+        }
+
+        loop2: while(true) {
+            System.out.println("Enter card suit: ");
+            cardSuit = input.nextInt();
+            switch (cardSuit) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    break loop2;
+            }
+        }
+
+
         //Then report the result here
+        if (cardSearching(cardValue, cardSuit, magicHand)) {
+            System.out.println("Your card is in the magic hand!");
+        } else {
+            System.out.println("Your card is not in the magic hand");
+        }
     }
-    
+
+    private static boolean cardSearching(int cValue, int cSuit, Card[] arr) {
+        for (Card  c : arr) {
+            if(c.getValue() == cValue && c.getSuit() == Card.SUITS[cSuit]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
