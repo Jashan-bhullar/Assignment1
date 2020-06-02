d7096a668e6a2dc869825110c57151a984ae837a
tranthe-sudo <66090435+tranthe-sudo@users.noreply.github.com>
2020-06-02 08:51:58 -0400

    Hardcode luckyCard to test

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -15,14 +15,15 @@ import java.util.Scanner;
  * Student ID: 991536629
  */
 public class CardTrick {
-
+    
     public static void main(String[] args)
     {
         int cardValue, cardSuit;
+
+        Card[] magicHand = new Card[7];
         Card luckyCard = new Card();
         luckyCard.setValue(5);
-        luckCard.setSuit("Hearts");
-        Card[] magicHand = new Card[7];
+        luckyCard.setSuit("Hearts");
 
         for (int i=0; i<magicHand.length; i++)
         {
@@ -32,54 +33,19 @@ public class CardTrick {
             magicHand[i] = c;
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        Scanner input = new Scanner(System.in);
-        loop: while(true) {
-            System.out.println("Enter card's value: ");
-            cardValue = input.nextInt();
-            switch(cardValue) {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                case 12:
-                case 13:
-                    break loop;
-                default:
-            }
-        }
-
-        loop2: while(true) {
-            System.out.println("Enter card suit: ");
-            cardSuit = input.nextInt();
-            switch (cardSuit) {
-                case 0:
-                case 1:
-                case 2:
-                case 3:
-                    break loop2;
-            }
-        }
 
 
         //Then report the result here
-        if (cardSearching(cardValue, cardSuit, magicHand)) {
-            System.out.println("Your card is in the magic hand!");
+        if (cardSearching(luckyCard.getValue(), luckyCard.getSuit(), magicHand)) {
+            System.out.println("You are one of lucky card owner!");
         } else {
-            System.out.println("Your card is not in the magic hand");
+            System.out.println("Sorry but yu are not one of the lucky card owner");
         }
     }
 
-    private static boolean cardSearching(int cValue, int cSuit, Card[] arr) {
+    private static boolean cardSearching(int cValue, String cSuit, Card[] arr) {
         for (Card  c : arr) {
-            if(c.getValue() == cValue && c.getSuit() == Card.SUITS[cSuit]) {
+            if(c.getValue() == cValue && c.getSuit() == cSuit) {
                 return true;
             }
         }
