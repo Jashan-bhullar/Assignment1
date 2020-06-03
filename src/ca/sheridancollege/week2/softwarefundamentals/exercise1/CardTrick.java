f5824b56bfac4f485335578df25bcb0dbc0a055c
shivampratapsingh1 <66032157+shivampratapsingh1@users.noreply.github.com>
2020-06-03 12:44:36 -0400

    first commit

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.week2.softwarefundamentals.exercise1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +22,53 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int) (Math.random() * (13) + 1));
+            int temp = (int)(Math.random()*4);
+            c.setSuit(c.SUITS[temp]);
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+         Scanner input = new Scanner(System.in);
+         
+         System.out.println("enter a number between 1 to 9");
+         int num = input.nextInt();
+         if(num>9)System.out.println("incorrect number entered");
+         System.out.println("enter the suitname");
+         String suitname = input.next();
+         
+         
+        Card luckyCard = new Card();
+        luckyCard.setValue(num);
+        luckyCard.setSuit(suitname);
+        int flag = 0;
+        for(int i=0;i<7;i++)
+        {
+            System.out.println(magicHand[i].getSuit()+" "+magicHand[i].getValue());
+            }
+        
+for(int i=0;i<7;i++){
+    if(luckyCard.getSuit().equalsIgnoreCase(magicHand[i].getSuit()) && luckyCard.getValue() == magicHand[i].getValue())
+    {
+        System.out.println("Congratulations, User's card is in the magic hand!!");
+    flag = 1;
+    break;
     }
-    
+    if(flag == 0)
+    {
+        System.out.println("sorry,User's card is not in the magic hand!!");
+    }
+
 }
+    }
+}
+        
+        
+   
+
+            
+        
+        
+       
+    
+    
+
